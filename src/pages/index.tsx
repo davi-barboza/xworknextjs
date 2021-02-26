b5b881f63c2f923486bcd651cb53efb34f3e9cdd
Davi Barboza <davicarlos_@hotmail.com>
2021-02-26 12:26:15 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
-import { Countdown } from "../components/CountDown";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
